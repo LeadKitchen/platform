dbaa615a0e44a91a802d3948dc48830def929d98
Maksim <kodermax@gmail.com>
2026-08-11 08:56:06 +0300

    up

--- apps/docs/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-import "./src/env";
-
-export default async function createNextConfig(): Promise<NextConfig> {
-  const config: NextConfig = {};
-
-  return config;
-}
--- apps/docs/postcss.config.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "@sitruk/tailwind-config/postcss-config";
