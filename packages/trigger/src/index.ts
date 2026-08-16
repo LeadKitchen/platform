65100664f2a08adf93bc37f3ea4a84cd8b4ff49c
Maksim <kodermax@gmail.com>
2026-08-16 20:31:48 +0300

    up

--- packages/trigger/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type { HelloWorldPayload } from "./tasks/hello-world";
-export { helloWorldTask } from "./tasks/hello-world";
-
-export { scheduledExampleTask } from "./tasks/scheduled-example";
